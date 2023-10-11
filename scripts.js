b22c9efea370debd4629f942984abd6b9a6a64e8
Saigowri <146852107+Bhupalamsaigowri@users.noreply.github.com>
2023-10-11 23:30:20 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,60 @@
+console.log("Welcome to the world guys");
+
+//h1 This is a comments
+console.log("hello sadan haridas");
+//variables
+let var1=6;
+let var2=12;
+console.log(var1+var2);
+let ageofaperson=8;
+let ageOfThePerson=22;
+let age_of_the_person=14;
+let ageoftheperson="sadan";
+let ageofthepersons="s";
+console.log(ageoftheperson);
+//constant
+let Dateofbirth=25;
+dateofbirth=28;
+console.log(dateofbirth);
+constantDateofbirth=25;
+dateofbirth=28;
+console.log(dateofbirth);
+//operators
+//aruthmetic operator
+let numb1=22;
+let numb2=44;
+console.log(numb1+numb2);
+console.log(numb1-numb2);
+console.log(numb1*numb2);
+console.log(numb1/numb2);
+console.log(numb1%numb2);
+//assignment operator
+let number1=5;
+let number2=4;
+console.log(number1+=number2);
+console.log(number1-=number2);
+console.log(number1*=number2);
+console.log(number1/=number2);
+console.log(number1%=number2);
+console.log(number1**=number2);
+//inc
+let age=12;
+age++;
+console.log(age);
+//dec
+let toys=33;
+toys--;
+console.log(toys);
+let bananas=12;
+bananas+=5;
+console.log(bananas);
+//comparison operator
+let number11=55;
+let number22=88;
+console.log(number11==number22);
+console.log(number11===number22);
+console.log(number11!=number22);
+console.log(number11<=number22);
+console.log(number11>=number22);
+console.log(number11<number22);
+console.log(number11>number22);
